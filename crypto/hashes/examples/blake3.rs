e752a0ae17d64ea280710bd28eb422aef90080bf
arthur@avatolabs.com <arthur@avatolabs.com>
2025-05-08 00:20:49 +0800

    fixed merkle tree root calculation in /body_validation_in_isolation.rs

--- crypto/hashes/examples/blake3.rs
+++ crypto/hashes/examples/blake3.rs
@@ -1,9 +1,14 @@
-use blake3::hash;
-
 fn main() {
-    let hash_empty = hash(b"");
-    let hash_abc = hash(b"abc");
+    let hash_hex = "46ecf45be3baca349dfe8a78deaf053b0aa6d538974da50fd6efb4d266bc8d21";
 
-    println!("Empty hash: {:?}", hash_empty);
-    println!("abc hash: {:?}", hash_abc);
+    // 将十六进制哈希字符串转为字节数组
+    let bytes = hex::decode(hash_hex).expect("Invalid hex string");
+
+    // 转换为 0x 格式
+    let formatted_hash = bytes.iter()
+        .map(|byte| format!("0x{:02x}", byte))
+        .collect::<Vec<String>>()
+        .join(", ");
+
+    println!("Formatted hash: {}", formatted_hash);
 }
