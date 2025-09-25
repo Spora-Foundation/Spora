a56d773465c473c457be1837e065c254c6db1ed5
Neo <wangyongzhao@avatolabs.com>
2025-09-25 09:36:01 +0800

    fix: pub tondi COIN_TYPE

--- wallet/keys/src/derivation/gen1/mod.rs
+++ wallet/keys/src/derivation/gen1/mod.rs
@@ -1,4 +1,4 @@
 //! Derivation management for the Tondi standard derivation scheme `'7890'`
 
 mod hd;
-pub use hd::{PubkeyDerivationManager, WalletDerivationManager};
+pub use hd::{PubkeyDerivationManager, WalletDerivationManager, COIN_TYPE};
